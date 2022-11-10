d495072c105472b9627a89738ca75e27db797fe6
Игорь <host0507@yandex.ru>
2022-11-10 20:08:13 +0300

    Класс Person перенесли в отдельный файл

---
+++ CL1/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CL1
+{
+    public class Person
+    {
+        public string namePerson;
+        public int age;
+
+        public Person()
+        {
+            namePerson = "X";
+            age = 0;
+        }
+
+        public Person(string name, int age)
+        {
+            namePerson = name;
+            this.age = age;
+        }
+    }
+}
--- CL1/Program.cs
+++ CL1/Program.cs
@@ -53,23 +53,7 @@ namespace CL1
 
     }
 
-    public class Person 
-    {
-        public string namePerson;
-        public int age;
-
-        public Person()
-        {
-            namePerson = "X";
-            age = 0;
-        }
-
-        public Person(string name, int age)
-        {
-            namePerson = name;
-            this.age = age;
-        }
-    }
+   
 
 
     internal class Program
